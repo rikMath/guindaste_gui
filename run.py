5629a8a33048b06947302dde4d03dc85cca6e79b
Matheus Brito Faria <matheus.brito000@gmail.com>
2022-04-30 19:58:59 -0300

    Add file to test run python interface

---
+++ run.py
@@ -0,0 +1,13 @@
+import time
+from Interface.CraneInterface import CraneInterface
+
+
+if __name__ == "__main__":
+
+    crane = CraneInterface()
+    crane.move_arm(10)
+    while True:
+        crane.move_crab(1)
+        time.sleep(5)
+        crane.move_crab(-1)
+        time.sleep(5)
